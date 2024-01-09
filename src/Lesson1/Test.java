ccac637a49aa0589d7d3d9d40037bcaf542b37e8
Дар'я Таневська <2daryachernata2@gmail.com>
2024-01-09 17:49:34 +0100

    new line

--- src/Lesson1/Test.java
+++ src/Lesson1/Test.java
@@ -3,6 +3,8 @@ package Lesson1;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello AQA-7");
+        int x = 1;
+        System.out.println(x);
         System.out.println("Hello AQA-7");
     }
 }
